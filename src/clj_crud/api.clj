2e9fc2c0b890e4701ee1bd35946afe0abf4b1542
Mussi <mussi@MacBook-Air-de-Mussi.local>
2019-07-20 21:24:07 -0300

    change app

--- src/clj_crud/api.clj
+++ src/clj_crud/api.clj
@@ -23,25 +23,21 @@
            (ok (crud/consultar)))
 
       (POST "/buscar" []
-           :return FindUser
            :summary "Busca cadastro na base."
            :body [user FindUser]
            (ok (crud/buscar (:id user))))
 
       (POST "/cadastrar" []
-            :return CadUser
             :summary "cadastrar um usuario"
             :body [user CadUser]
             (ok (crud/cadastrar  (:title user) (:description user))))
 
       (PUT "/editar" []
-            :return Edituser
             :summary "Atualizar um cadastro."
             :body [user Edituser]
             (ok (crud/editar (:id user) (:title user) (:description user))))
 
       (DELETE "/deletar" []
-            :return DeleteUser
             :summary "Deleta um usuario"
             :body [user DeleteUser]
             (ok (crud/deletar (:id user))))
